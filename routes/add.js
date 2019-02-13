41928b062231e0bed8f2f45f72229b1f6dd80623
Avi Cramer <abcramer@ucsd.edu>
2019-02-13 15:32:16 -0800

    adding dynamic json implementation

--- routes/add.js
+++ routes/add.js
@@ -2,12 +2,13 @@ var data = require("../data.json");
 
 exports.addEvent = function(request, response) {   
 	// Your code goes here
+	console.log(request.body.addEventForm);
 	var newEvent = {
 		"emotion": request.query.emotion,
 		"satisfaction": request.query.satisfaction,
 		"activity" : request.query.activity,
 		"location" : request.query.location,
-		"date" : request.query.data,
+		"date" : request.query.date,
 		"time" : request.query.time,
 		"notes" : request.query.notes
 	};
